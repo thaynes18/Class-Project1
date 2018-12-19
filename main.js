3bc127e567d040004e0b623a0e44f86f3d7cd5e1
Taneeya Haynes <thaynes@nyc.yearup.org>
2018-12-19 09:06:43 -0500

    Final Submission

---
+++ main.js
@@ -0,0 +1,13 @@
+function memberButton(){
+document.getElementById("clickthis").innerHTML= "You Are Now A Memeber";
+}
+
+// <button onclick="myFunction()">Click me</button>
+//
+// <p id="demo"></p>
+//
+// <script>
+// function myFunction() {
+//   document.getElementById("demo").innerHTML = "Hello World";
+// }
+// </script>
